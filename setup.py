2ea65091b684ea094fb83bc90f6df6b85b43f5db
Nico Schlömer <nico.schloemer@gmail.com>
2018-04-17 14:40:10 +0200

    better setup.py

--- setup.py
+++ setup.py
@@ -1,49 +1,49 @@
 # -*- coding: utf-8 -*-
 #
 import os
-from setuptools import setup
 import codecs
 
-from betterspy import __name__, __version__, __author__, __author_email__
+from setuptools import setup, find_packages
+
+# https://packaging.python.org/single_source_version/
+base_dir = os.path.abspath(os.path.dirname(__file__))
+about = {}
+with open(os.path.join(base_dir, 'betterspy', '__about__.py'), 'rb') as f:
+    # pylint: disable=exec-used
+    exec(f.read(), about)
 
 
 def read(fname):
-    try:
-        content = codecs.open(
-            os.path.join(os.path.dirname(__file__), fname),
-            encoding='utf-8'
-            ).read()
-    except Exception:
-        content = ''
-    return content
+    return codecs.open(os.path.join(base_dir, fname), encoding='utf-8').read()
+
 
 setup(
-    name=__name__,
-    version=__version__,
-    author=__author__,
-    author_email=__author_email__,
-    packages=['betterbib'],
-    description='Better BibTeX data',
-    long_description=read('README.rst'),
-    url='https://github.com/nschloe/betterbib',
-    download_url='https://pypi.python.org/pypi/betterbib',
-    license='License :: OSI Approved :: MIT License',
+    name='betterspy',
+    version=about['__version__'],
+    author=about['__author__'],
+    author_email=about['__author_email__'],
+    packages=find_packages(),
+    package_data={'betterspy': ['data/journals.json']},
+    description='Show sparsity pattern of sparse matrices',
+    long_description=read('README.md'),
+    long_description_content_type='text/markdown',
+    url=about['__website__'],
+    license=about['__license__'],
     platforms='any',
-    requires=[
-        'pybtex',
-        'requests'
+    install_requires=[
+        'matplotlib',
+        'numpy',
+        'pipdate',
         ],
     classifiers=[
-        'Development Status :: 4 - Beta',
+        about['__status__'],
+        about['__license__'],
         'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 3',
+        'Topic :: Scientific/Engineering',
         'Topic :: Utilities'
-        ],
-    scripts=[
-        'tools/betterbib'
         ]
     )
