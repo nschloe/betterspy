6520f37dfa717346b96fd178fb0c04241f7df8cd
Nico Schlömer <nico.schloemer@gmail.com>
2018-04-21 12:25:49 +0200

    add another test

--- test/test_betterspy.py
+++ test/test_betterspy.py
@@ -5,10 +5,10 @@ import tempfile
 
 import imageio
 import numpy
+import pytest
 from scipy import sparse
 
 import betterspy
-import pytest
 
 
 def test_show():
@@ -16,8 +16,7 @@ def test_show():
     betterspy.show(M)
     return
 
-@pytest.mark.parametrize(
-    'ref, kwargs', [
+@pytest.mark.parametrize('ref, kwargs', [
     (6967620, {}),
     (7502920, {'border_width': 1}),
     (21370785, {'border_width': 1, 'border_color': 'red'}),
@@ -38,5 +37,49 @@ def test_png(ref, kwargs):
     return
 
 
+# pylint: disable=too-many-locals
+def test_readme_images():
+    from dolfin import (
+        MeshEditor, Mesh, FunctionSpace, assemble, EigenMatrix, dot, grad, dx,
+        TrialFunction, TestFunction
+        )
+    import meshzoo
+
+    points, cells = meshzoo.rectangle(-1.0, 1.0, -1.0, 1.0, 20, 20)
+
+    # Convert points, cells to dolfin mesh
+    editor = MeshEditor()
+    mesh = Mesh()
+    # topological and geometrical dimension 2
+    editor.open(mesh, 'triangle', 2, 2, 1)
+    editor.init_vertices(len(points))
+    editor.init_cells(len(cells))
+    for k, point in enumerate(points):
+        editor.add_vertex(k, point[:2])
+    for k, cell in enumerate(cells.astype(numpy.uintp)):
+        editor.add_cell(k, cell)
+    editor.close()
+
+    V = FunctionSpace(mesh, 'CG', 1)
+    u = TrialFunction(V)
+    v = TestFunction(V)
+    L = EigenMatrix()
+    assemble(dot(grad(u), grad(v)) * dx, tensor=L)
+    A = L.sparray()
+
+    # M = A.T.dot(A)
+    M = A
+
+    with tempfile.TemporaryDirectory() as temp_dir:
+        filepath = os.path.join(temp_dir, 'test.png')
+        betterspy.write_png(filepath, M, border_width=2)
+
+    # betterspy.write_png(
+    #     'ATA.png', M, border_width=2,
+    #     colormap='viridis'
+    #     )
+    return
+
+
 if __name__ == '__main__':
-    test_png()
+    test_readme_images()
