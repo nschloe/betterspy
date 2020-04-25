e018b6a7a55aa935ff82eb6a9f20cf93c80d946b
Nico Schlömer <nico.schloemer@gmail.com>
2020-04-25 10:25:02 +0200

    black

--- betterspy/cli.py
+++ betterspy/cli.py
@@ -15,7 +15,7 @@ def _get_version_text():
                 sys.version_info.minor,
                 sys.version_info.micro,
             ),
-            "Copyright (c) 2018-2020 Nico Schlömer <nico.schloemer@gmail.com>"
+            "Copyright (c) 2018-2020 Nico Schlömer <nico.schloemer@gmail.com>",
         ]
     )
 
